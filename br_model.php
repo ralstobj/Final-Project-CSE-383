15c82c7bbe2a1f21ce39341b1ab7b22ed4abb0d0
ruthrab <ruthrab@ceclnx01.cec.miamioh.edu>
2018-12-05 11:33:13 -0500

    updates to php files

--- br_model.php
+++ br_model.php
@@ -2,9 +2,56 @@
 <?php
 // The data model for the BRsquared Project
 
+$mysqli = connectToDataBase();                          // create connection to database
+
 function getData($a) {
     $ret = array("data"=>strlen($a));
     return $ret;
+}
+
+/*
+Database:
+    ●	users: user table
+        ○	pk
+        ○	user
+        ○	password -> hashed using the php password_hash function
+        ○	timestamp
+    ●	diary: Item Entries
+        ○	pk
+        ○	userFK -> foreign Key to user - not the user but the pk of the user
+        ○	itemFK -> foreign Key to item. Not the item but the PK of the item
+        ○	timestamp
+    ●	diaryItems: list of items
+        ○	pk: int
+        ○	item: tinytext
+    ●	tokens
+        ○	pk
+        ○	user - actual user string
+        ○	token - token string created randomly
+        ○	timestamp
+*/
+
+
+// check if the provided user and password are correct
+function isUserAuth($user, $pass) {
+    // the SQL query for the info we want
+    $pass = password_hash($pass);
+    $isAuth = FALSE;
+
+    $theSQLstring = "SELECT pk
+                     from users
+                     where user='". $user ."' AND password='". $pass ."'";
+
+    $res = mysqli_query($mysqli, $theSQLstring);            // run and hold the results of the sql query
+
+    if (!$res) {
+        // there was an error with the database query
+        $isAuth = FALSE;
+    } else {
+        $isAuth = TRUE;
+    }
+
+    return $isAuth;
 }
 
 
--- rest.php
+++ rest.php
@@ -4,7 +4,12 @@
 
 require_once("br_model.php");                           // load in the data model for the project
 
-$mysqli = connectToDataBase();                          // creat connection to database
+//returns data as json
+function retJson($data) {
+    header('content-type: application/json');
+    print json_encode($data);
+    exit;
+}
 
 //get request method into $path variable
 $method = strtolower($_SERVER['REQUEST_METHOD']);
@@ -18,9 +23,10 @@ if (count($pathParts) <2) {
   $ret = array('status'=>'FAIL','msg'=>'Invalid URL');
   retJson($ret);
 }
-if ($pathParts[1] !== "v1") {
-  $ret = array('status'=>'FAIL','msg'=>'Invalid url or version');
-  retJson($ret);
+if ( $pathParts[1] !== "v1" || $pathParts[1] !== "items" ) {
+    // If they are not using v1 or looking specificly for rest.php/items/token fail out
+    $ret = array('status'=>'FAIL','msg'=>'Invalid url or version');
+    retJson($ret);
 }
 
 //get json data if any
@@ -35,60 +41,115 @@ try {
 } catch (Exception $e) {
 };
 
-//look for url /v1/user
-if ($method==="post" && count($pathParts) ==  3 && $pathParts[2] === "user") {
-    /*
-    ■	Given user and password will get a token validating the user. If no user is present or the password does not match will return status will == "FAIL"
-    ■	passwords are hashed using the php password_hash function
-    ■	url: rest.php/v1/user
-    ■	method: post
-    ■	json_in:
-        ●	user
-        ●	password
-    ■	json_out
-        ●	status: "OK" or "FAIL"
-        ●	msg:
-        ●	token: string
-    ■	Test:
-        ●	curl -X 'POST' -d '{"user":"test","password":"test"}' https://ceclnx01.cec.miamioh.edu/~campbest/cse383/finalProject/restFinal.php/v1/user
-    */
-    /*
-    $status = "OK";                                         // test for status of data pull
-    $data = array();                                        // will hold the returned results from teh sql query
-    $theSQLstring = "SELECT keyName from KeyValue";         // the SQL query for the info we want
-    $res = mysqli_query($mysqli, $theSQLstring);            // run and hold the results of the sql query
 
-    if (!$res) {
-        // there was an error with the database query
-        $status = "FAIL";
-    } else {
-        // Loop around the results row by row and add the data to the $data array
-        while($row = mysqli_fetch_assoc($res)) {
-            $data[] = $row;
-        }
+
+// Get Token - rest.php/v1/user
+if ($method==="post" && count($pathParts) == 3 && $pathParts[1] === "v1" && $pathParts[2] === "user") {
+    /*
+    Given user and password will get a token validating the user.
+    If no user is present or the password does not match will return status will == "FAIL"
+    passwords are hashed using the php password_hash function
+
+    json_in:
+        user
+        password
+    json_out
+        status: "OK" or "FAIL"
+        msg:
+        token: string
+    Test:
+        curl -X 'POST' -d '{"user":"test","password":"test"}' https://ceclnx01.cec.miamioh.edu/~campbest/cse383/finalProject/restFinal.php/v1/user
+    */
+   
+    // make sure we have we have the correct JSON information we need to make the updated
+    if (!isset($jsonData['user']) || !isset($jsonData['password'])) {
+        $ret = array('status'=>'FAIL','msg'=>'json is invalid','token'=>'');
+        retJson($ret);
     }
 
-    $ret = array('status'=>$status,'data' => $data);        // build the array we want to convert to JSON
+    // we were provided a user and password now we check if they are good
+    if isUserAuth($user, $pass) {
+        // generate and return the token
+        $ret = array('status'=>'OK','msg'=>'','token'=> getToken() );
+        retJson($ret);
+    }
+
+    $ret = array('status'=>'FAIL', 'msg' =>'Username and Password not found','token'=>'');        // build the array we want to convert to JSON
     retJson($ret);                                          // convert the the return array to JSON
+}
+
+
+// Get list of items - rest.php/v1/items
+if ($method==="get" && count($pathParts) == 3 && $pathParts[1] === "v1" && $pathParts[2] === "items") {
+    /*
+    Return the set of items we are tracking and their key
+    json_in: none
+    json_out:
+        status
+        msg
+        items[]
+            pk
+            item
+    test:
+        https://ceclnx01.cec.miamioh.edu/~campbest/cse383/finalProject/restFinal.php/v1/items
+        curl https://ceclnx01.cec.miamioh.edu/~campbest/cse383/finalProject/restFinal.php/v1/items
     */
 }
 
+
+// Get Items User Consumed - rest.php/items/token
+if ($method==="get" && count($pathParts) == 3 && $pathParts[1] === "items") {
 /*
-    ■	Return the set of items we are tracking and their key
-    ■	rest.php/v1/items
-    ■	method: get
-    ■	json_in: none
-    ■	json_out:
-        ●	status
-        ●	msg
+    ■	Call gets the tracked items for a given user
+    ■	limit to last 30 items
+    ■	JSON Response:
+        ●	status: OK or AUTH_FAIL or FAIL
+        ●	msg: text
         ●	items[]
             ○	pk
             ○	item
-    ■	test:
-        ●	https://ceclnx01.cec.miamioh.edu/~campbest/cse383/finalProject/restFinal.php/v1/items
-        ●	curl https://ceclnx01.cec.miamioh.edu/~campbest/cse383/finalProject/restFinal.php/v1/items
-
+            ○	timestamp
+    ■	test
+        ●	https://ceclnx01.cec.miamioh.edu/~campbest/cse383/finalProject/restFinal.php/v1/items/1db4342013a7c7793edd72c249893a6a095bca71
 */
+}
 
+
+// Get Summary of Items - rest.php/v1/itemsSummary/token
+if ($method==="get" && count($pathParts) == 4 && $pathParts[1] === "v1" && $pathParts[2] === "itemsSummary") {
+/*
+    ■	json_in: none
+    ■	json_out
+        ●	status
+        ●	msg
+        ●	items[]
+            ○	item
+            ○	count
+    ■	test
+        ●	https://ceclnx01.cec.miamioh.edu/~campbest/cse383/finalProject/restFinal.php/v1/itemsSummary/1db4342013a7c7793edd72c249893a6a095bca71
+*/
+}
+
+
+// Update Items Consumed - rest.php/v1/items
+if ($method==="post" && count($pathParts) == 3 && $pathParts[1] === "v1" && $pathParts[2] === "items") {
+/*
+    ■	Updates item as being consumed
+    ■	JSON IN
+        ●	token: string token
+        ●	ItemFK: <key>
+    ■	JSON OUT
+        ●	status: OK or AUTH_FAIL or FAIL
+        ●	msg: text
+    ■	test
+        ●	curl -X 'POST' -d '{"token":"1db4342013a7c7793edd72c249893a6a095bca71","itemFK":2}' https://ceclnx01.cec.miamioh.edu/~campbest/cse383/finalProject/restFinal.php/v1/items
+*/
+}
+
+
+function genToken() {
+    // creat a user token to be stored in the data base
+    // then pass the token to the model to store it
+}
 
 ?>
