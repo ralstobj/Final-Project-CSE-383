b21890a5b5a2ccdb1b9d52f9fa292ce80f07e0c9
Bailey Ralston <ralstobj@miamioh.edu>
2018-12-05 11:23:23 -0500

    Added ajax to send password to php file.

--- br_main.js
+++ br_main.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
     $("#alert").hide();
 });
+
+function validateIdentity() {
+    var dataFromForm = {};
+    dataFromForm.user = $("#user").val();
+    dataFromForm.password = $("#password").val();
+    $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "rest.php",
+            contentType: 'application/json',
+            data: JSON.stringify(dataFromForm),
+            success: function(data){
+                console.log(data);
+                $("#alert").hide();
+            },
+            error: function(e){
+                console.log(e.message);
+                $("#alert").show();
+            }
+    });
+}
+
